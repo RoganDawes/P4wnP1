ef0df161765ccd64a1e1d6a7e85cc8de794513a7
MaMe82 <mame8282@googlemail.com>
2018-01-30 16:52:42 +0000

    Added haveged to installer, in order to cope with hostapd entropy issue (Station couldn't connect)

--- install.sh
+++ install.sh
@@ -66,7 +66,8 @@ sudo apt-get -y upgrade # include patched bluetooth stack
 #fi
 
 # hostapd gets installed in even if WiFi isn't present (SD card could be moved from "Pi Zero" to "Pi Zero W" later on)
-sudo apt-get -y install dnsmasq git python-pip python-dev screen sqlite3 inotify-tools hostapd autossh bluez bluez-tools bridge-utils ethtool  policykit-1 genisoimage tcpdump iodine
+# Note: added haveged to cope with entropy problem of hostapd
+sudo apt-get -y install dnsmasq git python-pip python-dev screen sqlite3 inotify-tools hostapd autossh bluez bluez-tools bridge-utils ethtool  policykit-1 genisoimage tcpdump iodine haveged
 
 
 # at this point the nameserver in /etc/resolv.conf is set to 127.0.0.1, so we replace it with 8.8.8.8
